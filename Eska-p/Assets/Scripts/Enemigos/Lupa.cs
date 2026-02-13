d9394d6e41de916dd41ab00ca51277d2cb76ed21
Oliver <oliver_herrero@euneiz.com>
2026-02-13 18:30:32 +0100

    ataque de la lupa hecho

---
+++ Eska-p/Assets/Scripts/Enemigos/Lupa.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Lupa : MonoBehaviour
+{
+    private bool isAttacking = false;
+
+    void Start()
+    {
+        isAttacking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("player") && isAttacking == false)
+        {
+            isAttacking = true;
+            collision.gameObject.GetComponent<Player>().TakeDamage(1); // Hace dos de da�o al jugador
+            StartCoroutine("Attack");
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1f);
+        isAttacking = false;
+    }
+}
