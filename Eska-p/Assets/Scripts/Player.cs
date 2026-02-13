6c87c61951b9295fd2b34a3f97be8e8c98e29730
Oliver <oliver_herrero@euneiz.com>
2026-02-13 15:44:23 +0100

    Revert "whiteboxing nivel 4 y he arreglaod lo de los enemigos de accesibilidad"
    This reverts commit 0ee1c1b295b938e296116342be3fdc3caafb361a.
    Da conflicto

--- Eska-p/Assets/Scripts/Player.cs
+++ Eska-p/Assets/Scripts/Player.cs
@@ -4,14 +4,13 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-    private int attack = 1;
-    public int health;
-    //public bool receivingDamage = false;
+    public int attack = 1;
+    public int health = 3;
+    private bool receivingDamage = false;
 
     void Start()
     {
-        Time.timeScale = 1f;  // El juego se incia, esto deberá estar en gamemanager pero por ahora aqui
-        health = 3;
+        Time.timeScale = 1f;  // El juego se inciia, esto deberá estar en gamemanager pero por ahora aquí
     }
 
     // Update is called once per frame
@@ -22,31 +21,28 @@ public class Player : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        Debug.Log("tofavia no quitó");
-        //receivingDamage = false;
-        //if (!receivingDamage)
-        //{
-            Debug.Log("quitó");
+        if (!receivingDamage)
+        {
             health -= damage; // Se le quita la cantidad de daño a la cantidad de vida
             if (health <= 0) // Si tiene 0 o menos vida
             {
                 Die(); // Llama al método de muerte
             }
-            /*else
+            else
             {
                 receivingDamage = true;
                 DeactivateDamage();
-            } */     
-        //}
+            }      
+        }
     }
 
-    /*public void DeactivateDamage()
+    public void DeactivateDamage()
     {
         if (receivingDamage)
         {
             StartCoroutine("Damage");
         }
-    }*/
+    }
 
     public void OnTriggerEnter(Collider other) 
     {
@@ -58,13 +54,12 @@ public class Player : MonoBehaviour
 
     public void Die()
     {
-        Debug.Log("muerte");
         Time.timeScale = 0f;
     }
 
-    /*IEnumerator Damage() // Corrutina para que no pueda recibir daño doble al mismo momento, igual hay que auemntar los segundosen el futuro
+    IEnumerator Damage() // Corrutina para que no pueda recibir daño doble al mismo momento, igual hay que auemntar los segundosen el futuro
     {
         yield return new WaitForSeconds(1f); 
         receivingDamage = false; 
-    }*/
+    }
 }
