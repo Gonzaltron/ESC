551b3717c5de3bd2f29059d89b0eaaca1b28aaa4
Naiara <naiara_igartua@euneiz.com>
2026-02-17 11:27:36 +0100

    Hay qe tener cuidao con la gravity

--- Eska-p/Assets/Scripts/Lexie/playercontroller.cs
+++ Eska-p/Assets/Scripts/Lexie/playercontroller.cs
@@ -6,24 +6,49 @@ using System.Collections;
 public class playercontroller : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float Gravity = -9;
+
     private Vector3 movementDirection;
     private Vector3 moveVel;
     private Rigidbody rb;
     private bool onground = true;
+    private Vector3 velVertical;
+    private CharacterController controlador;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
+    }
+    private void Awake()
+    {
+        controlador = GetComponent<CharacterController>();
     }
 
     void Update()
     {
+        MovimientoNormal();
+        Gravedad();
+
+    }
+    private void MovimientoNormal()
+    {
+        //Teclas
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
         float sal = Input.GetAxis("Jump");
 
         movementDirection = new Vector3(hor * speed, sal * speed, ver * speed);
         transform.position += movementDirection * speed * Time.deltaTime;
+        controlador.Move(movementDirection);
+
+    }
+    private void Gravedad()
+    {
+        velVertical.y += Gravity * Time.deltaTime;
+        controlador.Move(velVertical * Time.deltaTime);
+
 
     }
 
