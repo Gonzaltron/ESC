cab51b85bd5d3b68ca804bcd26b83d3728e0d78d
Naiara <naiara_igartua@euneiz.com>
2026-02-27 17:29:40 +0100

    Si

--- Eska-p/Assets/Scripts/Lexie/playercontroller.cs
+++ Eska-p/Assets/Scripts/Lexie/playercontroller.cs
@@ -33,7 +33,6 @@ public class playercontroller : MonoBehaviour
     void Update()
     {
         MovimientoNormal();
-        //Gravedad();
         //SaltoBombastic();
 
     }
@@ -41,14 +40,6 @@ public class playercontroller : MonoBehaviour
     
     public void MovimientoNormal()
     {
-        //Teclas
-        /*float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
-        float sal = Input.GetAxis("Jump");
-
-        movementDirection = new Vector3(hor * speed, sal* speed, ver * speed);
-        transform.position = movementDirection * speed * Time.deltaTime;
-        controlador.Move(movementDirection);*/
         if (Input.GetKey(KeyCode.W))
         {
             //this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
@@ -72,39 +63,8 @@ public class playercontroller : MonoBehaviour
             Debug.Log("Derecha");
         }
     }
-    private void Gravedad()
-    {
-        velVertical.y += Gravity * Time.deltaTime;
-        controlador.Move(velVertical * Time.deltaTime);
-    }
-    /*public IEnumerator SaltoBombastic()
-    {
-        
-        if (Input.GetKeyDown(KeyCode.E) && !onground)
-        {
-            yield return new WaitForSeconds(arribaTime);
-            rb.AddForce(Vector3.down * fCaida, ForceMode.Impulse);
-
-        }
-        
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        while (collision.gameObject.CompareTag("Ground"))
-        {
-            onground = true;
-            Debug.Log("Esta en el suelo");
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        while (collision.gameObject.CompareTag("Ground"))
-        {
-            onground = false;
-            Debug.Log("Esta en el aire");
-
-        }
-    }*/
+    
+    
+    
 
 }
