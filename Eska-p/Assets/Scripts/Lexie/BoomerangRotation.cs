b85907dba8752695004192858e7116b7178d750f
Naiara <naiara_igartua@euneiz.com>
2026-03-08 23:29:20 +0100

    Boomerang terminado

--- Eska-p/Assets/Scripts/Lexie/BoomerangRotation.cs
+++ Eska-p/Assets/Scripts/Lexie/BoomerangRotation.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class BoomerangRotation : MonoBehaviour
 {
+    public float speedX;
+    public float speedY;
+    public float speedZ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,6 @@ public class BoomerangRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(speedX, speedY, speedZ);
     }
 }
