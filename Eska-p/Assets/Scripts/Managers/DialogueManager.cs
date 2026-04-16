5707321f700359b9672be480aea9c831a74e11fe
Oliver <oliver_herrero@euneiz.com>
2026-04-16 13:53:05 +0200

    Ya está el diálogo con los triggers y se puede quitar, todavía se ve mal pero en ell oestoy

---
+++ Eska-p/Assets/Scripts/Managers/DialogueManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            InkManager.Instance.EndDialogue();
+        }
+    }
+}
--- Eska-p/Assets/Scripts/Managers/InkManager.cs
+++ Eska-p/Assets/Scripts/Managers/InkManager.cs
@@ -1,21 +1,31 @@
 using Ink.Runtime;
 using TMPro;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Rendering;
 public class InkManager : MonoBehaviour
 {
     public TextMeshProUGUI dialogueText;
     public TextAsset inkJSON;
-
+    public GameObject dialoguePanel;
+    public static InkManager Instance;
     private Story story;
     void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
         story = new Story(inkJSON.text);
     }
-
+    
     public void StartDialogue(string knotName)
     {
         story.ChoosePathString(knotName);
+       
         ContinueStory();
     }
 
@@ -23,7 +33,17 @@ public class InkManager : MonoBehaviour
     {
         if (story.canContinue)
         {
+            Time.timeScale = 0f;
+            dialoguePanel.SetActive(true);
             dialogueText.text = story.Continue();
+            
         }
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("a");
+        dialoguePanel.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
