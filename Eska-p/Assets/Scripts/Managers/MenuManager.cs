8a178d5ba818c258c94026e49b0c26bcc494db81
Naiara <naiara_igartua@euneiz.com>
2026-05-07 17:51:03 +0200

    Main Menu

---
+++ Eska-p/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Jugar()
+    {
+        SceneManager.LoadScene("AllLevels");
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
