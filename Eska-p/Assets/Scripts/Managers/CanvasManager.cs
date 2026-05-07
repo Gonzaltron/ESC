e586ea4b11e86843b5e5d6aacd8312b68777f618
Naiara <naiara_igartua@euneiz.com>
2026-05-07 20:46:43 +0200

    Menu de pausa

--- Eska-p/Assets/Scripts/Managers/CanvasManager.cs
+++ Eska-p/Assets/Scripts/Managers/CanvasManager.cs
@@ -4,52 +4,57 @@ using UnityEngine.SceneManagement;
 
 public class CanvasManager : MonoBehaviour
 {
-    [SerializeField] Canvas mainMenu;
-    [SerializeField] Canvas game;   
-    [SerializeField] Canvas pause;
-    Canvas prev;
 
-    
+    public GameObject pauseCanvas;
+    public GameObject gameCanvas;
+
+    public void Awake()
+    {
+        pauseCanvas.SetActive(false);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        mainMenu.enabled = true;
-        game.enabled = false;
-        Cursor.visible = true;
-        pause.enabled = false;
-        Time.timeScale = 0;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
 
+        Time.timeScale = 0;
     }
+
+    private bool isPaused = false;
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Pause();
+            if (isPaused) Seguir();
+            else Pause();
         }
 
     }
     public void Pause()
     {
-        game.enabled = false;
-        pause.enabled = true;
-        Time.timeScale = 0;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
+        isPaused = true;
+        Time.timeScale = 0f;           // Para el juego
+        pauseCanvas.SetActive(true);
+        if (gameCanvas != null) gameCanvas.SetActive(false);
     }
-    public void Game()
+    public void Seguir()
     {
-        mainMenu.enabled = false;
-        game.enabled = true;
-        Time.timeScale = 1;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        isPaused = false;
+        Time.timeScale = 1f;           
+        pauseCanvas.SetActive(false);
+        if (gameCanvas != null) gameCanvas.SetActive(true);
+    }
+    public void Empezar()
+    {
+        Time.timeScale = 1f;           //Resetea el juego desde 0
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     public void MainMenu()
     {
-        SceneManager.LoadScene("AllLevels");
+        Time.timeScale = 1f;    
+        SceneManager.LoadScene("MainMenu");
     }
 }
